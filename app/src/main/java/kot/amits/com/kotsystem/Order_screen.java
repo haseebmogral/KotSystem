30e59bfab6cf336f512ba3032b4f34e52904812d
Haseeb mogral <haseebmogral@gmail.com>
2018-10-25 16:23:39 +0530

    java adapter changed with working adapter and model

--- app/src/main/java/kot/amits/com/kotsystem/Order_screen.java
+++ app/src/main/java/kot/amits/com/kotsystem/Order_screen.java
@@ -64,14 +64,13 @@ public class Order_screen extends AppCompatActivity {
 
         Album1 a;
 
-        a = new Album1("2");
-        albumList.add(a);
 
-        a = new Album1("5");
-        albumList.add(a);
 
-        a = new Album1("3");
-        albumList.add(a);
+
+
+        for (int i = 0; i < 15; i++) {
+            a = new Album1(String.valueOf(i));
+            albumList.add(a);        }
 
 
         adapter.notifyDataSetChanged();
