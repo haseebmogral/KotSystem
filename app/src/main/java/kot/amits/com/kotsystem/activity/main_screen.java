a366c059735eb4179e604324fa7de8f04c72b75e
Thasreef <amthasreefmmogral@gmail.com>
2018-11-01 21:52:52 +0530

    added menu layout

--- app/src/main/java/kot/amits/com/kotsystem/activity/main_screen.java
+++ app/src/main/java/kot/amits/com/kotsystem/activity/main_screen.java
@@ -4,7 +4,16 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import kot.amits.com.kotsystem.DBhelper.DBmanager;
 import kot.amits.com.kotsystem.R;
@@ -12,29 +21,44 @@ import kot.amits.com.kotsystem.R;
 public class main_screen extends AppCompatActivity {
     DBmanager dBmanager;
     Button start;
+    CarouselView carouselView;
+
+    int[] sampleImages = {R.drawable.maxresdefault, R.drawable.circular_button_s};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
 
-        dBmanager=new DBmanager(this);
+        carouselView = (CarouselView) findViewById(R.id.carouselView);
+        carouselView.setPageCount(sampleImages.length);
+
+        carouselView.setImageListener(imageListener);
+
+
+        dBmanager = new DBmanager(this);
         dBmanager.open();
 
-        start=(Button)findViewById(R.id.start_order);
-
-
-        start.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-               dBmanager.CART_ID= String.valueOf(dBmanager.add_to_cart_details(dBmanager.get_date(),dBmanager.get_time()));
-                Intent intent=new Intent(main_screen.this, Order_screen.class);
-                startActivity(intent);
-            }
-        });
-
+//        start   = (Button) findViewById(R.id.start_order);
+//
+//
+//        start.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                dBmanager.CART_ID = String.valueOf(dBmanager.add_to_cart_details(dBmanager.get_date(), dBmanager.get_time()));
+//                Intent intent = new Intent(main_screen.this, Order_screen.class);
+//                startActivity(intent);
+//            }
+//        });
 
 
     }
+
+    ImageListener imageListener = new ImageListener() {
+        @Override
+        public void setImageForPosition(int position, ImageView imageView) {
+            imageView.setImageResource(sampleImages[position]);
+        }
+    };
 }
