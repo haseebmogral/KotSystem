327fdd192d9c5ec9cc26d1f1234afddfbd811a56
Thasreef <amthasreefmmogral@gmail.com>
2018-11-03 10:24:38 +0530

    aapt2 eror solved 02-11-2018

--- app/src/main/java/kot/amits/com/kotsystem/activity/Order_screen.java
+++ app/src/main/java/kot/amits/com/kotsystem/activity/Order_screen.java
@@ -85,14 +85,14 @@ public class Order_screen extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_order_screen);
 
         total_textview =(TextView)findViewById(R.id.total_amount);
-        order =(Button) findViewById(R.id.order);
-        order.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //space to order place in database
-                mydb.place_order(cart_list);
-            }
-        });
+//        order =(Button) findViewById(R.id.order);
+//        order.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                //space to order place in database
+//                mydb.place_order(cart_list);
+//            }
+//        });
 //        searchView =(SearchView) findViewById(R.id.searchview);
 
 //        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
