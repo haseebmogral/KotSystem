891b46bc4cd5c25ae86250445cbd1737910a0520
Haseeb mogral <haseebmogral@gmail.com>
2018-12-07 10:20:42 +0530

    28-11-2018 10:57 PM

--- app/src/main/java/kot/amits/com/kotsystem/activity/manage_sales.java
+++ app/src/main/java/kot/amits/com/kotsystem/activity/manage_sales.java
@@ -1,6 +1,9 @@
 package kot.amits.com.kotsystem.activity;
 
+import android.app.DatePickerDialog;
+import android.content.DialogInterface;
 import android.database.Cursor;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
@@ -10,10 +13,14 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import java.util.Locale;
 
 import kot.amits.com.kotsystem.DBhelper.DBHelper;
 import kot.amits.com.kotsystem.DBhelper.DBmanager;
@@ -100,7 +107,45 @@ public class manage_sales extends AppCompatActivity {
             //Back button
 
             case R.id.filter:
-                //addfav (heart icon) was clicked, Insert your after click code here.
+                String[] colors = {"Date", "Type"};
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("filter by options");
+                builder.setItems(colors, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                       if (which==0){
+                           final Calendar myCalendar = Calendar.getInstance();
+
+                           DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+                               @Override
+                               public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                                     int dayOfMonth) {
+                                   // TODO Auto-generated method stub
+                                   myCalendar.set(Calendar.YEAR, year);
+                                   myCalendar.set(Calendar.MONTH, monthOfYear);
+                                   myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                               }
+
+                           };
+
+                           new DatePickerDialog(manage_sales.this, date, myCalendar
+                                   .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                                   myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+
+                           String myFormat = "dd-MM-yyyy"; //In which you need put here
+                           SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+                           setTitle(sdf.format(myCalendar.getTime()));
+
+                       }
+                       else if (which==1){
+
+                       }
+                    }
+                });
+                builder.show();
                 return true;
 
             default:
